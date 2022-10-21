b7ad93d2a61d458b01b698055f3d5ee1733ef70e
akmalsyrf <dev.akmal69@gmail.com>
2022-10-22 00:22:36 +0700

    update mock-data

--- mock-data.js
+++ mock-data.js
@@ -2,6 +2,13 @@ let newsData = {news: [
     { id: "1", title: "Note 1", body: "Content 1", postImage: "Post image 1" },
     { id: "2", title: "Note 2", body: "Content 2", postImage: "Post image 2" },
     { id: "3", title: "Note 3", body: "Content 3", postImage: "Post image 3" },
+    { id: "4", title: "Note 4", body: "Content 4", postImage: "Post image 4" },
+    { id: "5", title: "Note 5", body: "Content 5", postImage: "Post image 5" },
+    { id: "6", title: "Note 6", body: "Content 6", postImage: "Post image 6" },
+    { id: "7", title: "Note 7", body: "Content 7", postImage: "Post image 7" },
+    { id: "8", title: "Note 8", body: "Content 8", postImage: "Post image 8" },
+    { id: "9", title: "Note 9", body: "Content 9", postImage: "Post image 9" },
+    { id: "10", title: "Note 10", body: "Content 10", postImage: "Post image 10" },
 ]};
 
 module.exports = newsData
